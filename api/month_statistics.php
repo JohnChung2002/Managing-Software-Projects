44938d4d397993112e71f5fa8eb9749d98199ba8
John Chung <101917927+JohnChung2002@users.noreply.github.com>
2022-10-14 23:45:48 +0800

    Create reporting APIs

---
+++ api/month_statistics.php
@@ -0,0 +1,31 @@
+<?php
+    require_once "api_functions.php";
+
+    function loadYearMonth($month_name, $year) {
+        $statistics = array();
+        $conn = start_connection();
+        $command = "SELECT appointment_date, COUNT(booking_id) AS number_of_bookings FROM booking_info WHERE MONTHNAME(appointment_date)=? AND YEAR(appointment_date)=? AND booking_status='Confirmed' GROUP BY (appointment_date);";
+        $stmt = mysqli_prepare($conn, $command);
+        mysqli_stmt_bind_param($stmt, "si", $month_name, $year);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $statistics[$row["appointment_date"]] = (int)$row["number_of_bookings"];
+        }
+        mysqli_free_result($result);
+        mysqli_close($conn);
+        return $statistics;
+    }
+
+    date_default_timezone_set("Asia/Kuala_Lumpur");
+    if (!empty($_GET["month_name"]) && !empty($_GET["year"])) {
+        $month_name = $_GET["month_name"];
+        $year = $_GET["year"];
+        if (validate_monthname($month_name) && validate_year($year)) {
+            echo json_encode(loadYearMonth($month_name, $year));
+            exit;
+        }
+    }
+    http_response_code(400);
+    exit;
+?>
---
+++ api/year_statistics.php
@@ -0,0 +1,47 @@
+<?php
+    require_once "api_functions.php";
+
+    function loadYear($year) {
+        $statistics = array();
+        $conn = start_connection();
+        $command = "SELECT MONTHNAME(appointment_date) AS appointment_month, COUNT(booking_id) AS number_of_bookings FROM booking_info WHERE YEAR(appointment_date)=? AND booking_status='Confirmed' GROUP BY MONTHNAME(appointment_date);";
+        $stmt = mysqli_prepare($conn, $command);
+        mysqli_stmt_bind_param($stmt, "i", $year);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $statistics[$row["appointment_month"]] = (int)$row["number_of_bookings"];
+        }
+        mysqli_free_result($result);
+        mysqli_close($conn);
+        return $statistics;
+    }
+
+    function loadAll() {
+        $statistics = array();
+        $conn = start_connection();
+        $command = "SELECT CONCAT(MONTHNAME(appointment_date), ' ', YEAR(appointment_date)) AS appointment_month, COUNT(booking_id) AS number_of_bookings FROM booking_info WHERE booking_status='Confirmed' GROUP BY CONCAT(MONTHNAME(appointment_date), ' ', YEAR(appointment_date));";
+        $result = mysqli_query($conn, $command);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $statistics[$row["appointment_month"]] = (int)$row["number_of_bookings"];
+        }
+        mysqli_free_result($result);
+        mysqli_close($conn);
+        return $statistics;
+    }
+
+    date_default_timezone_set("Asia/Kuala_Lumpur");
+    if (!empty($_GET["year"])) {
+        $year = $_GET["year"];
+        if (validate_year($year)) {
+            echo json_encode(loadYear($year));
+            exit;
+        }
+    } else {
+        echo json_encode(loadAll());
+        exit;
+    }
+    http_response_code(400);
+    exit;
+
+?>

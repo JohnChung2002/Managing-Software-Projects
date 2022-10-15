982b528ef0aa2f510ec900b2af3aaa595d902547
John Chung <101917927+JohnChung2002@users.noreply.github.com>
2022-10-15 12:51:28 +0800

    Modularise header and footer

---
+++ footer.php
@@ -0,0 +1,37 @@
+<?php
+echo '
+<section class="">
+    <footer class="text-white text-center fixed-bottom" style="background-color: #1AA36D;" > 
+        <div class="container p-4" >
+            <div class="row">
+                <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Cacti-Succulent Kuching</h5>
+                    <p>
+                        We are a local company that specializes in selling only the best, high quality cacti and other planting products. Our goal is to deliver the best to the customer.
+                    </p>
+                </div>
+                <div class="col-lg-5 col-md-0 mb-0 mb-md-0">
+                    <h5 class="text-uppercase">Business Hours</h5>
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                        <p class="text-white">Weekday:	9:00 AM – 6:00 PM</p>
+                        </li>
+                        <li>
+                        <p class="text-white">Weekend:	9:00 AM – 3:00 PM</p>
+                        </li>
+                        <li>
+                        <p class="text-white">Public Holiday:	9:00 AM – 3:00 PM</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+        <div class="text-center p-3" style="background-color: #1AA36D;">
+        © 2022 Copyright:
+        <a class="text-white" href="https://cactisucculentkuching.cf/">cactisucculentkuching.cf/</a>
+        </div>
+    </footer>
+</section>
+';
+
+?>

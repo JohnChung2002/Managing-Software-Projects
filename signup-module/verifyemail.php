f42a8737cf3f0d41639ea275e859115b686ef528
Gabriel Kuek <101234169@students.swinburne.edu.my>
2022-10-18 01:35:28 +0800

    Updated the function to check for token expiry
    - Update the function to check if the token expired, if it is expired send another verification link with new token to the user.
    - Changed the MySQL query to use Binds variables to a prepared statement as parameters.

--- signup-module/verifyemail.php
+++ signup-module/verifyemail.php
@@ -1,18 +1,37 @@
 <?php
     require_once '../database_credentials.php'; // File of the database credentials PATH MAYBE UPDATED
+    include 'send_email.php';
 
     if(isset($_GET['token'])){
         $token = $_GET['token'];
         $conn = mysqli_connect($servername, $username, $password, $database);
-        $sql = "SELECT * FROM user_credentials WHERE account_token = '$token' LIMIT 1";
-        $result = mysqli_query($conn, $sql);
+        $sql = $conn -> prepare("SELECT * FROM user_credentials WHERE account_token = ? LIMIT 1;");
+        $sql->bind_param('s', $token);
+        $sql->execute();
+        $result = $sql->get_result();
         if(mysqli_num_rows($result) > 0){
             $user = mysqli_fetch_assoc($result);
-            $update_query = "UPDATE user_credentials SET account_token = '', account_status = 'Activated', token_expiry = '' WHERE email_address = '$user[email_address]'";
-            if(mysqli_query($conn, $update_query)){
+            $email = $user['email_address'];
+            $tokenExpiry = $user['token_expiry'];
+            $status = 'Activated';
+            $emptyVal = " ";
+
+            if(date("Y-m-d H:i:s") < date($tokenExpiry)){
+                $stmt = $conn -> prepare("UPDATE user_credentials SET account_token = ?, account_status = ?, token_expiry = ? WHERE email_address = ?");
+                $stmt->bind_param('ssss', $emptyVal, $status, $emptyVal, $email);
+                $stmt->execute();
                 echo "Your email has been verified successfully.";
+                $stmt->close();
             }else{
-                echo "Something went wrong. Please try again.";
+                echo "Your token has expired. Please check your email again.";
+                // Generate random 12 characters hex
+                $randHex = bin2hex(random_bytes(11));
+                // Generate token expiry
+                $tokenExpiry = date("Y-m-d H:i:s", strtotime("+5 minutes")); // 5 minutes from now
+                $stmt = $conn -> prepare("UPDATE user_credentials SET account_token = ?, token_expiry = ? WHERE email_address = ?");
+                $stmt->bind_param('sss', $randHex, $tokenExpiry, $email);
+                $stmt->execute();
+                sendAccountVerificationEmail($email, $randHex);
             }
         }else{
             echo "Invalid token.";

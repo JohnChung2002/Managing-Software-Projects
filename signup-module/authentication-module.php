5f25667e01a0dc0c94b892cb6f45d85a7f98c763
Gabriel Kuek <101234169@students.swinburne.edu.my>
2022-10-18 01:31:43 +0800

    modified the array to named array

--- signup-module/authentication-module.php
+++ signup-module/authentication-module.php
@@ -32,6 +32,6 @@
             $hash = hashPasswordDatabase($password);
             $bool = true;
         }
-        return array($hash, $bool);
+        return array("hashedPassword"=>$hash, "is_integrity"=>$bool);
     }
 ?>

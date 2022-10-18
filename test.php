c9cd6bd7027640089e0fa92800c30665e52d0400
John Chung <101917927+JohnChung2002@users.noreply.github.com>
2022-10-18 21:27:13 +0800

    Update styling

--- test.php
+++ test.php
@@ -7,26 +7,28 @@
 </head>
 <body>
     <?php include 'header.php'; ?>
-    <h1 class="text-center mt-5">Visitor Pattern Report</h1>
-    <h2 class="text-center mt-5">Daily Report</h2>
-    <hr />
-    <h3 class="text-center mt-5">Time Based Report</h3>
-    <div class="text-center mt-5">
-        <canvas id="TimeChart"></canvas>
-    </div>
-    <h3 class="text-center mt-5">Day Based Report</h3>
-    <div class="text-center mt-5">
-        <canvas id="DayChart"></canvas>
-    </div>
-    <h2 class="text-center mt-5">Monthly Report</h2>
-    <hr />
-    <div class="text-center mt-5">
-        <canvas id="MonthChart"></canvas>
-    </div>
-    <h2 class="text-center mt-5">Yearly Report</h3>
-    <hr />
-    <div class="text-center mt-5">
-        <canvas id="YearChart"></canvas>
+    <div class="container">
+        <h1 class="text-center mt-5">Visitor Pattern Report</h1>
+        <h2 class="text-center mt-5">Daily Report</h2>
+        <hr />
+        <h3 class="text-center mt-5">Time Based Report</h3>
+        <div class="text-center mt-5">
+            <canvas id="TimeChart"></canvas>
+        </div>
+        <h3 class="text-center mt-5">Day Based Report</h3>
+        <div class="text-center mt-5">
+            <canvas id="DayChart"></canvas>
+        </div>
+        <h2 class="text-center mt-5">Monthly Report</h2>
+        <hr />
+        <div class="text-center mt-5">
+            <canvas id="MonthChart"></canvas>
+        </div>
+        <h2 class="text-center mt-5">Yearly Report</h3>
+        <hr />
+        <div class="text-center mt-5">
+            <canvas id="YearChart"></canvas>
+        </div>
     </div>
     <script src="script/chart.js"></script>
     <script>

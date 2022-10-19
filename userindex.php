8d425fe68ee52b20f433455d7fd8e25f9ff49735
Zasha11 <113078511+Zasha11@users.noreply.github.com>
2022-10-19 21:08:05 +0800

    two new headers

---
+++ userindex.php
@@ -0,0 +1,45 @@
+<?php
+echo '<header>
+        <nav class="navbar navbar-expand-lg" style="background-color: #1AA36D;">
+            <div class="container-fluid mx-auto">
+              <img src="images/cactus.png" class="me-4" height="90"
+              alt="Cactus-Succlent Kuching Logo" loading="lazy" />
+              <a class="navbar-brand fw-bold fs-1" style ="color: white;" href="#">Cacti-Succulent Kuching</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse"  data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon" style="color: white;"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                  <ul class="navbar-nav nav-pills nav-fill mb-4 mb-lg-0 ">
+                    <li class="nav-item">
+                      <a class="nav-link " href="#"><p class ="fs-5 mx-3 mt-3" style="color: white;">Booking</p></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        <p class ="fs-5 mx-3 mt-3" style="color: white;">Plant Encyclopedia</p>
+                    </a>
+                    </li>
+                    <li class="nav-item dropdown me-5">
+                        <a class="nav-link dropdown-toggle mt-2 me-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img
+                                src="https://cdn-icons-png.flaticon.com/512/64/64572.png"
+                                class="rounded-circle mx-3"
+                                height="45"
+                                alt="Black and White Portrait of a Man"
+                                loading="lazy"
+                            />
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="#">My Profile</a></li>
+                          <li><a class="dropdown-item" href="#">Another action</a></li>
+                          <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">Sign Out</a></li>
+                        </ul>
+                     </li>
+                </ul>
+              </div>
+            </div>
+            </div>
+        </nav>
+          
+    </header>';
+?>

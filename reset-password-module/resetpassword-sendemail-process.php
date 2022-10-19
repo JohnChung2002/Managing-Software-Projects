edf9bd79d7c333571dc33a193aeb3a07f7d28110
Gabriel Kuek <101234169@students.swinburne.edu.my>
2022-10-19 17:05:59 +0800

    Send email to user for password reset link

---
+++ reset-password-module/resetpassword-sendemail-process.php
@@ -0,0 +1,44 @@
+<?php
+    require_once 'database_credentials.php'; // File of the database credentials PATH MAYBE UPDATED
+    include 'send_email.php';
+    
+    // Set the session for message
+    session_start();
+
+    $email = "";
+
+    if(isset($_POST['email'])){
+        $email = $_POST['email'];
+        checkUserAndSendEmail($servername, $username, $password, $database, $email);
+    }
+
+    // Function to check if the user exists in the database, and send a link to their email.
+    function checkUserAndSendEmail($servername, $username, $password, $database, $email){
+        // Initialise connection to database
+        $conn = mysqli_connect($servername, $username, $password, $database);
+
+        // Check if email exists within the database
+        $sql = $conn -> prepare("SELECT * FROM user_credentials WHERE email_address = ?;");
+        $sql->bind_param('s', $email);
+        $sql->execute();
+        $result = $sql->get_result();
+        if(mysqli_num_rows($result) > 0){
+            sendResetPasswordEmail($email);
+            $_SESSION['rstpassMsg'] = "
+                <div class='alert alert-success'>
+                    Check your email for a link to reset your password.
+                </div>
+                ";
+        }else{
+            // EMAIL DOES NOT EXIST or Account not activated
+            $_SESSION['rstpassMsg'] = "
+                <div class='alert alert-danger'>
+                    Email does not exist in the database.
+                </div>
+                ";
+        }
+
+        // Close connection
+        mysqli_close($conn);
+    }
+?>

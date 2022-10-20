85f0dbbd0d3224b6814e7943d317704d638bd58e
John Chung <101917927+JohnChung2002@users.noreply.github.com>
2022-10-20 18:14:06 +0800

    Update reference in dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,5 @@
 <?php 
-include "login-module/is_login.php";
+include "login-module/is_loggedin.php";
 echo "Logged in: " . $_SESSION["user_id"]; 
 echo "Logged in role: " . $_SESSION["user_role"];
 ?>

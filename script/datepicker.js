826bc8a90590246876286dc4209094552b2722e5
John Chung <101917927+JohnChung2002@users.noreply.github.com>
2022-10-22 19:46:22 +0800

    Fix merge conflict

--- script/datepicker.js
+++ script/datepicker.js
@@ -81,9 +81,9 @@ function retrieveSlots(input_date) {
 function updateSlots(date) {
     retrieveSlots(date).then(function(data) {
         var availableSlots = data;
-        $('#available-slot').empty()
+        $('#time').empty()
         for (var i = 0; i < availableSlots.length; i++) {
-            $('#available-slot').append(new Option(availableSlots[i], availableSlots[i]))
+            $('#time').append(new Option(availableSlots[i], availableSlots[i]))
         }
         hideNextPrevMonthDates();
     }).catch(err => console.log(err));

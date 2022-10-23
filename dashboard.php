32591b43b47fea46a008d4650c0044eb733cb955
John Chung <101917927+JohnChung2002@users.noreply.github.com>
2022-10-23 19:30:17 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -6,10 +6,6 @@ include "auth/is_loggedin.php";
 <head>
     <?php include "page_head.php";?>
     <title>Booking Dashboard</title>
-    <!--javascript for bootstrap-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <!--css for bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
   <?php include 'header.php'; ?>

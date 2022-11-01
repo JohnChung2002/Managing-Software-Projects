2964905c1e2a7605115a0738425dc28442d545e5
brendanjap <100980077+brendanjap@users.noreply.github.com>
2022-11-01 21:07:36 +0800

    Visitor_encyclopedia

---
+++ visitor_encyclopedia.php
@@ -0,0 +1,47 @@
+<?php 
+include "auth/is_loggedin.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "page_head.php";?>
+    <title>Gardening Encyclopedia</title>
+</head>
+<body>
+  <?php include 'header.php'; ?>
+  <div class="container p-5 my-5 bg-success">
+  <h1 class="text-center text-white">Gardening Encyclopedia</h1>
+ 
+   <div class="col">
+      <div class="card p-3 my-5 border">
+		<img src="images/Cactus3.png">
+		<br>
+		<h2 class="text-center"> Explore plants, gardening tools and fertilizer</h2>
+		<br>
+		<p>
+		Garden tools are an important part of garden supplies. 
+		The right garden tools can make taking care of your garden easy and productive.
+		Not all garden tools are equal. You want tools that you can use on a daily basis that are economically fashioned. 
+		Some tools are made for people with large hands, while others are crafted for smaller hands. 
+		Ergonomic tools may have silicone grip handles or handles with easy grip indentations. 
+		There are specific garden tools for the various tasks that are part of gardening. 
+		For example, specific garden tools and supplies are designed for vegetable, herbs, and flower gardening. 
+		Whether you have a raised bed garden or a field garden, you will need these hand tools. 
+		A few of these tools include, hand weeder, trowel, transplanter, bulb planter, hand fork, hand rake, garden seeder, spray bottle, carry bag, two or more pairs of gardening gloves, carry tote, cultivator, kneeling pad, and other tools. 
+		Pest control requires various tools, such as sprayers and perhaps protective masks. 
+		If you're using chemical sprays, you'll want protective clothing, goggles, and a hat to prevent blowback from changing winds. 
+		You'll need separate containers for different types of chemical products, such as fungicides, fertilizers, specific insect sprays. If you're an organic gardener, you won't require the kind of pest control tools and supplies.
+		</p>
+		
+	  </div>
+   </div>
+
+
+    
+
+
+ 
+  </div>
+  <?php include 'footer.php'; ?>
+</body>
+</html>

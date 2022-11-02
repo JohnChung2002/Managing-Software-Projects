ee002f329bdcbdf9ace4e6968287b43d23169ffa
Zasha11 <113078511+Zasha11@users.noreply.github.com>
2022-11-02 15:12:13 +0800

    Create pageeditor.php

---
+++ pageeditor.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include 'page_head.php'; ?>
+        <title>Cacti Succulent Kuching</title>
+    </head>
+
+    <body>
+        <?php include 'footer.php'; ?>
+    </body>
+</html>

9a0908202c38a7a6535ee8c0860b45e05672fc1e
Zasha11 <113078511+Zasha11@users.noreply.github.com>
2022-11-04 03:03:58 +0800

    Update pageeditor.php

--- pageeditor.php
+++ pageeditor.php
@@ -16,6 +16,11 @@
         if (isset($_POST["mytextarea"])) {	 
             echo "You entered: " . $_POST["mytextarea"];
         }
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+          }
         ?>
 
         <form method="POST">

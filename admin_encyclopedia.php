fe285991e23f8b91715f9fa21a9956e2b38eea72
sherenachan <99006664+sherenachan@users.noreply.github.com>
2022-11-03 09:51:24 +0800

    brendan's template

---
+++ admin_encyclopedia.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "page_head.php";?>
+    <title>Gardening Encyclopedia</title>
+</head>
+<body>
+  <?php include 'header.php'; ?>
+  <div class="d-block w-100 h-100">
+  <img src="images/Cactus8.jpg" class="img-fluid">
+  </div>
+  <div class="container p-5 my-5" style="background-color: #1AA36D;">
+  <h1 class="text-center text-white">Gardening Encyclopedia</h1>
+ 
+   <div class="col">
+      <div class="card p-3 my-5 border">
+		<img src="images/Cactus4.png">
+		<br>
+		<h4 class="text-center"> Explore plants, gardening tools and fertilizer</h4>
+		<br>
+		<p>
+		Garden tools are an important part of garden supplies. 
+		The right garden tools can make taking care of your garden easy and productive.
+		Not all garden tools are equal. You want tools that you can use on a daily basis that are economically fashioned. 
+		Some tools are made for people with large hands, while others are crafted for smaller hands. 
+		Ergonomic tools may have silicone grip handles or handles with easy grip indentations. 
+		There are specific garden tools for the various tasks that are part of gardening. 
+		For example, specific garden tools and supplies are designed for vegetable, herbs, and flower gardening. 
+		Whether you have a raised bed garden or a field garden, you will need these hand tools. 
+		A few of these tools include, hand weeder, trowel, transplanter, bulb planter, hand fork, hand rake, garden seeder, spray bottle, carry bag, two or more pairs of gardening gloves, carry tote, cultivator, kneeling pad, and other tools. 
+		Pest control requires various tools, such as sprayers and perhaps protective masks. 
+		If you're using chemical sprays, you'll want protective clothing, goggles, and a hat to prevent blowback from changing winds. 
+		You'll need separate containers for different types of chemical products, such as fungicides, fertilizers, specific insect sprays. If you're an organic gardener, you won't require the kind of pest control tools and supplies.
+		</p>
+		
+<br>
+<div class="card-group">
+  <div class="card">
+    <img src="images/Cactus1.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Plants</h5>
+      <p class="card-text">
+		Plants are multicellular organisms in the kingdom Plantae that use photosynthesis to make their own food. 
+		There are over 300,000 species of plants; common examples of plants include grasses, trees, and shrubs. 
+		Plants have an important role in the world’s ecosystems. 
+		They produce most of the world’s oxygen, and are important in the food chain, as many organisms eat plants or eat organisms which eat plants. 
+		The study of plants is called botany.
+	  </p>
+	</div>
+	  <a href="#" class="btn btn-primary stretched-link">Search for plants</a>
+  </div>
+  <div class="card">
+    <img src="images/Cactus2.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Gardening Tools</h5>
+      <p class="card-text">
+	  A gardening tool or garden tool is any one of many tools made for gardens and gardening and overlaps with the range of tools made for agriculture and horticulture. 
+	  Garden tools can also be hand tools and power tools.
+	  </p>
+	</div>
+	  <a href="#" class="btn btn-primary stretched-link">Search for gardening tools</a>
+  </div>
+  <div class="card">
+    <img src="images/Cactus3.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Fertilizer</h5>
+      <p class="card-text">
+	  A natural or artificial material called fertiliser is added to the soil or plants to promote growth and productivity. 
+	  They give the plants nutrients.
+	  </p>
+    </div>
+	<a href="#" class="btn btn-primary stretched-link">Search for fertilizer</a>
+  </div>
+</div>
+	
+	  </div>
+   </div>
+  </div>
+  <?php include 'footer.php'; ?>
+</body>
+</html>

421645939fc62f268fba6930655486a37c4703ad
John Chung <101917927+JohnChung2002@users.noreply.github.com>
2022-11-03 22:07:07 +0800

    Update API link

--- api_credentials.php
+++ api_credentials.php
@@ -3,6 +3,6 @@ if ( basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"]) ) {
     http_response_code(403);
     exit;
 } 
-$api_link = "https://script.google.com/macros/s/AKfycbzP6VALt9woWgjLGz2RaKsPx6gpmIabMEe_bMg4QsFmkzoMLY-P37vZAhsHeO6L8oM/exec";
+$api_link = "https://script.google.com/macros/s/AKfycbyaA2cUuZvksku91HjtJG4zs-ds9Z-I3A2Kh9CTJY52vI3D8Jq6S86UO79avgTN/exec";
 $api_key = "EB3914D9F167D9A414DF438C7D4CD";
 ?>

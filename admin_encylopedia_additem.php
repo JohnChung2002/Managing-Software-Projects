4b92304b1eb199df094b36860e62397b724d846e
sherenachan <99006664+sherenachan@users.noreply.github.com>
2022-11-04 21:19:36 +0800

    rename file

---
+++ admin_encylopedia_additem.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "page_head.php";?>
+    <title>Gardening Encyclopedia</title>
+</head>
+<body>
+  <?php include 'header.php'; ?>
+    <div class="d-block w-100 h-100">
+
+    </div>
+  <?php include 'footer.php'; ?>
+</body>
+</html>

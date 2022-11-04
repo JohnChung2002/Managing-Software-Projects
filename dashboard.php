2c438dcbb575b1c7ca7b979daccb7f1cd812e928
brendanjap <100980077+brendanjap@users.noreply.github.com>
2022-11-04 20:50:31 +0800

    visitor_dashboard

--- dashboard.php
+++ dashboard.php
@@ -8,13 +8,47 @@ include "auth/is_loggedin.php";
     <title>Booking Dashboard</title>
 </head>
 <body>
-  <?php include 'header.php'; ?>
-  <div class="d-block w-100 h-100">
-  <img src="images/Cactus7.jpg" class="mx-auto d-block w-100">
-  </div>
-  <div class="container p-5 my-5" style="background-color: #1AA36D;">
-  <h1 class="text-center text-white">Booking Dashboard</h1>
-  <div class="row row-cols-1 row-cols-md-2 g-4">
+ <?php include 'header.php'; ?>
+     <div id="CactusBanner" class="carousel slide" data-bs-ride="false">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#CactusBanner" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#CactusBanner" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="images/Cactiplant1.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="images/Cactus4.png" class="d-block w-100 h-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#CactusBanner" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#CactusBanner" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+	
+<div class="container border border-success rounded my-5">
+   <div class="row text-center">  
+        <div class=" w-100 my-3">
+          <h1 class="text-uppercase my-3">Booking Dashboard</h1>
+		  <img src="images/Cactiplant1.jpg">
+		<br>
+        </div>
+   </div>      
+  <div class="row row-cols-md-2">
     <?php 
     if($_SESSION["user_role"] == "Admin") {
       echo '
@@ -30,7 +64,7 @@ include "auth/is_loggedin.php";
       </div>';
     }?>
     <div class="col">
-      <div class="card p-5 my-5 border">
+      <div class="card p-5 my-2 border">
         <img src="images/Cactus5.jpeg" class="card-img-top w-60" alt="...">
         <div class="card-body">
           <h5 class="card-title">APPOINTMENT REQUEST</h5>
@@ -40,7 +74,7 @@ include "auth/is_loggedin.php";
       </div>
     </div>
     <div class="col">
-      <div class="card p-5 my-5 border">
+      <div class="card p-5 my-2 border">
         <img src="images/Cactus6.jpeg" class="card-img-top w-60" alt="...">
         <div class="card-body">
           <h5 class="card-title">MANAGE YOUR BOOKING</h5>
@@ -49,12 +83,11 @@ include "auth/is_loggedin.php";
       </div>
       </div>
     </div>
-
     <?php 
     if($_SESSION["user_role"] == "Admin") {
       echo '
       <div class="col">
-      <div class="card p-5 my-5 border">
+      <div class="card p-5 my-2 border">
         <img src="images/Cactus6.jpeg" class="card-img-top w-60" alt="...">
         <div class="card-body">
           <h5 class="card-title">SEARCH BOOKING</h5>
@@ -64,7 +97,7 @@ include "auth/is_loggedin.php";
       </div>
     </div>
     <div class="col">
-      <div class="card p-5 my-5 border">
+      <div class="card p-5 my-2 border">
         <img src="images/Cactus6.jpeg" class="card-img-top w-60" alt="...">
         <div class="card-body">
           <h5 class="card-title">CALENDAR BOOKING VIEW</h5>
@@ -76,7 +109,10 @@ include "auth/is_loggedin.php";
     }
     ?>
   </div>
+    </div>
   </div>
+</div>
+
   <?php include 'footer.php'; ?>
 </body>
 </html>

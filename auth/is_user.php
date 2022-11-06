367b1db403ad8a0e242f5cc2cc71e3fb9bd25f69
John Chung <101917927+JohnChung2002@users.noreply.github.com>
2022-11-06 18:36:53 +0800

    Update session start catch

--- auth/is_user.php
+++ auth/is_user.php
@@ -4,7 +4,10 @@
         exit;
     }
 
-    session_start();
+    if(!isset($_SESSION)) { 
+        session_start(); 
+    } 
+    
     if(isset($_SESSION['is_login']) && $_SESSION['is_login'] == true){
         if ($_SESSION['user_role'] == "User") {
             return true;

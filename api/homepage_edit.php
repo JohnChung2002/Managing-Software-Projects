246eccf8905f5a1a6b2379d83af907788454cc63
Zasha11 <113078511+Zasha11@users.noreply.github.com>
2022-11-07 01:09:27 +0800

    Update homepage_edit.php

--- api/homepage_edit.php
+++ api/homepage_edit.php
@@ -1,4 +1,12 @@
 <?php
+
+    require_once dirname(__FILE__)."/api_functions.php";
+
+    if (!is_admin()) {
+        http_response_code(400);
+        exit;
+    }
+
     function Setcontent() {
         var ContentSet = document.getElementById("page_resource_button");
        tinymce.get('page_resource').setContent(contentOne);

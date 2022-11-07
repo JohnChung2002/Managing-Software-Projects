7e99f56ee8cc20bce2e6b59c9c89f0f1471f5251
Gabriel Kuek <101234169@students.swinburne.edu.my>
2022-11-07 22:04:02 +0800

    Check if user is superadmin

---
+++ auth/is_superadmin.php
@@ -0,0 +1,16 @@
+<?php
+    if ( basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"]) ) {
+        http_response_code(403);
+        exit;
+    }
+
+    session_start();
+    if(isset($_SESSION['is_login']) && $_SESSION['is_login'] == true){
+        if ($_SESSION['user_role'] == "Super Admin") {
+            return true;
+        } 
+    }
+    
+    header("Location: login.php");
+    exit;
+?>

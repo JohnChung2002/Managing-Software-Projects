36e37f0b49c4a42dffe147021dffea6b1a0675c4
Zasha11 <113078511+Zasha11@users.noreply.github.com>
2022-11-10 22:16:55 +0800

    Update confirmdelete.php

---
+++ homepage-promotion/confirmdelete.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../page_head.php'; ?>
+    <title>Cacti Succulent Kuching</title>
+</head>
+<body id = 'version_id'>
+    <?php include '../database_credentials.php'; ?>
+    
+    <h1> Are you sure you want to delete this content?</h1>
+
+    <button type="button" class="btn btn-success position-absolute top-70 start-50 translate-middle mt-1">
+        <a class="text-white-50" href="/Managing-Software-Projects/pageeditor.php"> Go Back </a>
+    </button>
+     
+</body>
+</html>

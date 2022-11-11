f31a4c124e23b88e9bec796c7c044efc117d4d75
sherenachan <99006664+sherenachan@users.noreply.github.com>
2022-11-11 19:47:16 +0800

    update edit form

--- admin_encylopedia_edititem.php
+++ admin_encylopedia_edititem.php
@@ -16,61 +16,26 @@
   <?php include 'header.php'; ?>
   <div class="container border border-success rounded my-5">
         <div class="col-4 w-100">
-          <h1 class="text-uppercase my-4 text-center">Edit Existing Item</h1>
-
-      
+          <?php 
+            include "encyclopedia_functions.php";
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+              editEncylopediaItem();
+            } else {
+                echo"
+                <h1 class='text-uppercase my-4 text-center'>Edit Existing Item</h1>
           <br/>
-          <aside class="float-end">
+          <aside class='float-end'>
               Instructions for uploading image: <br/>
-                1. Head over to <a target=”_blank” href="https://imgur.com/upload">Imagur</a> to upload an image. <br/>
+                1. Head over to <a target=”_blank” href='https://imgur.com/upload'>Imagur</a> to upload an image. <br/>
                 2. Upload the image/copy the link of the image from online and paste it in the box. <br/>
                 3. Hover over the image to show the copy link button and click it. <br />
-                4. Paste it in to the "Item Image" field and add .jpeg behind the link!
-          </aside>
-          <div class="px-2">
-          <form action="admin_encyclopedia_addtodatabase.php" method="post" novalidate="novalidate">
-            <fieldset>
-              <label>Product Name: <input type="text" name="productname" id="productname" maxlength="50" required="required"></label><br />
-              <br />
-              <label for="icategory">Item Category:</label>
-                <select id="icategory" name="icategory" required="required">
-                  <option value="">Choose</option>
-                  <option value="Compost & Soil">Compost & Soil</option>
-                  <option value="Fertiliser">Fertiliser</option>
-                  <option value="Pesticides">Pesticides</option>
-                  <option value="Pots & Vases">Pots & Vases</option>
-                  <option value="Tools & Accessories">Tools & Accessories</option>
-                  <option value="Seeds">Seeds</option>
-                  <option value="Plants">Plants</option>
-                </select>
-              <br />
-              <br />
-              <label>Item Subcategory: <input type="text" name="isubcategory" id="isubcategory" maxlength="30" required="required"></label><br />
-              <br />
-              <label>Item Image (Link Only): <input type="text" name="iimage" id="iimage" maxlength="50" required="required"></label><br />
-              <br />
-                <p>Item Availability In Store: </p>
-                <div class="form-check form-check-inline">
-                  <input type="radio" id="available" name="iavailability" value="Available">
-                  <label for="available">Available</label><br>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input type="radio" id="notavailable" name="iavailability" value="Not Available">
-                  <label for="notavailable">Not Available</label><br>
-                </div>
-                </br>
-                </br>
-              <label>Price In Store: <input type="text" name="iprice" id="iprice" required="required"></label><br />
-              <br />
-              <label>Description: <input type="text" name="idesc" id="idesc" required="required"></label><br />
-            </fieldset>
-            <br />
-            <div id="button">
-              <input type="submit" class="buttons" value="Submit">        
-              <input type="reset" class="buttons" value="Reset">
-            </div>
-		      </form>
-          </div>
+                4. Paste it in to the 'Item Image' field and add .jpeg behind the link!
+          </aside>";
+                retrieveFormDetails($_GET["id"]);
+                
+            }
+            
+        ?>
           <br />
         </div>
     </div>
--- encyclopedia_functions.php
+++ encyclopedia_functions.php
@@ -71,4 +71,105 @@ function deleteEncylopediaItem() {
     </div>";
     return false;
 }
+
+function retrieveFormDetails($item_id){
+    require_once 'database_credentials.php'; // File of the database credentials PATH MAYBE UPDATED
+	$conn = new mysqli($servername, $username, $password, $database);
+    $command = "SELECT item_id, item_category, item_subcategory, item_name, item_image, availability_in_store, price_in_store, description FROM encyclopedia_items WHERE item_id=?";
+    $stmt = mysqli_prepare($conn, $command);
+    mysqli_stmt_bind_param($stmt, "i", $item_id);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $row = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+    echo "
+    <div class='px-2'>
+    <form method='post' novalidate='novalidate'>
+            <fieldset>
+              <label>Product Name: <input type='text' name='productname' id='productname' required='required' value='".$row["item_name"]."'></label><br />
+              <br />
+              <label for='icategory'>Item Category:</label>
+                <select id='icategory' name='icategory' required='required'>
+                  <option value='".$row['item_category']."'>".$row['item_category']."</option>
+                  <option value=''>Choose</option>
+                  <option value='Compost & Soil'>Compost & Soil</option>
+                  <option value='Fertiliser'>Fertiliser</option>
+                  <option value='Pesticides'>Pesticides</option>
+                  <option value='Pots & Vases'>Pots & Vases</option>
+                  <option value='Tools & Accessories'>Tools & Accessories</option>
+                  <option value='Seeds'>Seeds</option>
+                  <option value='Plants'>Plants</option>
+                </select>
+              <br />
+              <br />
+              <label>Item Subcategory: <input type='text' name='isubcategory' id='isubcategory' maxlength='30' required='required' value='".$row['item_subcategory']."'></label><br />
+              <br />
+              <label>Item Image (Link Only): <input type='text' name='iimage' id='iimage' maxlength='50' required='required' value='".$row['item_image']."'></label><br />
+              <br />
+                <p>Item Availability In Store: </p>
+                <div class='form-check form-check-inline'>
+                  <input type='radio' id='available' name='iavailability' value='Available'>Available<br/>
+                </div>
+                <div class='form-check form-check-inline'>
+                  <input type='radio' id='notavailable' name='iavailability' value='Not Available'>Not Available<br/>
+                </div>
+                </br>
+                </br>
+              <label>Price In Store: <input type='text' name='iprice' id='iprice' required='required' value='".$row['price_in_store']."'></label><br />
+              <br />
+              <label>Description: <input type='text' name='idesc' id='idesc' required='required' value='".$row['description']."'></label><br />
+            </fieldset>
+            <br />
+            <div id='button'>
+              <input type='submit' class='buttons' value='Submit'>        
+              <input type='reset' class='buttons' value='Reset'>
+            </div>
+		      </form>
+              </div>";
+}
+
+function editEncylopediaItem() {
+    if (!empty($_GET["id"])) {
+        $item_id = $_GET["id"];
+        require_once 'database_credentials.php'; // File of the database credentials PATH MAYBE UPDATED
+	    $conn = new mysqli($servername, $username, $password, $database);
+        $command = "SELECT item_id, item_category, item_subcategory, item_name, item_image, availability_in_store, price_in_store, description FROM encyclopedia_items WHERE item_id=?";
+        $stmt = mysqli_prepare($conn, $command);
+        mysqli_stmt_bind_param($stmt, "i", $item_id);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        $row = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
+
+        $productname = $_POST['productname'];
+        $icategory = $_POST['icategory'];
+        $isubcategory = $_POST['isubcategory'];
+        $iimage = $_POST['iimage'];
+        $iavailability = $_POST['iavailability'];
+        $iprice = $_POST['iprice'];
+        $idesc = $_POST['idesc'];
+        
+        $command = "UPDATE encyclopedia_items SET item_category= '$icategory', item_subcategory= '$isubcategory', item_name= '$productname', item_image= '$iimage', availability_in_store= '$iavailability', price_in_store= '$iprice', description= '$idesc' WHERE item_id=?";
+        $stmt = mysqli_prepare($conn, $command);
+        mysqli_stmt_bind_param($stmt, "i", $item_id);
+        if (mysqli_stmt_execute($stmt)) {
+            echo "
+            <div class='container min-vh-100'>
+                <div class='alert alert-success mt-4'>
+                Item edited successfully!
+                </div>
+            </div>";
+            mysqli_close($conn);
+            return true;
+        }
+        mysqli_close($conn);
+    }
+    echo "
+    <div class='container min-vh-100'>
+        <div class='alert alert-danger'>
+        Invalid request. Please try again.
+        </div>
+    </div>";
+    return false;
+}
 ?>

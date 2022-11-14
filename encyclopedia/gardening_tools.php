a08f5eae433d8bfb706c031f426535508ef7a6d9
John Chung <101917927+JohnChung2002@users.noreply.github.com>
2022-11-14 12:26:54 +0800

    Merge visitor encyclopedia module to main (#35)
    * Visitor_encyclopedia
    
    * visitor_encyclopedia interface
    
    * Update path
    
    * Updated visitor_encyclopedia
    
    * Display gardening encyclopedia(visitor)
    
    * banyak-banyak kaktus
    
    * Update view_encyclopedia
    
    * Update visitor_encyclopedia
    
    * 1+1=2
    
    * Update visitor_encyclopedia
    
    * Update encyclopedia interface
    
    * kaktus hilang
    
    * visitor_display_encyclopedia
    
    * Update visitor_encyclopedia interface
    
    * update display_encyclopedia
    
    * update display_encyclopedia
    
    * Update visitor_encyclopedia_interface
    
    * visitor_dashboard
    
    * update visitor_dashboard
    
    * update visitor_encyclopedia_interface
    
    * Update visitor_booking_dashboard
    
    * visitor_tools_encyclopedia
    
    * kaktus
    
    * plant_encyclopedia
    
    * Visitor_search_plant
    
    * Search encyclopedia item
    
    * Update search_encyclopedia
    
    * Update interface
    
    * Update dashboard
    
    * Sort plant, sort seed, sort price
    
    * sort gardening things
    
    * sort fertilizer
    
    * Update sort
    
    * hover itu kaktus
    
    * Deobfuscate background image
    
    * Plant_encyclopedia
    
    * gardening_tools_encyclopedia
    
    * baja_ensiklopedia
    
    * encyclopedia_details
    
    * Finalize Encyclopedia
    
    * Update
    
    * Update
    
    Co-authored-by: brendanjap <100980077+brendanjap@users.noreply.github.com>

---
+++ encyclopedia/gardening_tools.php
@@ -0,0 +1,65 @@
+<form action="" method="GET" class="d-flex">
+    <input class="form-control me-3" type="search" placeholder="Item ID" 
+	aria-label="Search" name="item_id" value="
+<?php 
+	if(isset($_GET['item_id'])){
+		echo $_GET['item_id'];
+		}?>">
+<button class="btn btn-dark" type="submit">Search</button>
+</form>
+<?php 
+require_once 'database_credentials.php'; // File of the database credentials PATH MAYBE UPDATED
+$con = new mysqli($servername, $username, $password, $database);
+if(isset($_GET['item_id'])){
+    $item_id = $_GET['item_id'];
+	$query = "SELECT * FROM encyclopedia_items WHERE (item_category='Tools & Accessories' OR item_category='Pesticides' OR item_category='Pots & Vases') AND item_id='$item_id'";
+	
+    $query_run = mysqli_query($con, $query);
+if(mysqli_num_rows($query_run) > 0){
+    foreach($query_run as $row){
+?>
+<br>
+<div class="container p-5" style="background-color: white;">
+<div class="container w-50 p-3">
+<img class='img-fluid' src="<?= $row['item_image'];?>">
+</div>
+<div class="list-group w-50 mx-auto">
+  <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+    <div class="d-flex w-100 justify-content-center">
+      <h5 class="mb-1">Item Name:</h5>
+    </div>
+    <p class="mb-1 text-center"><?= $row['item_name']; ?></p>
+  </a>
+  <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+    <div class="d-flex w-100 justify-content-center">
+      <h5 class="mb-1">Availability In Store:</h5>
+    </div>
+    <p class="mb-1 text-center"><?= $row['availability_in_store']; ?></p>
+  </a> 
+  <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+    <div class="d-flex w-100 justify-content-center">
+      <h5 class="mb-1">Item Subcategory:</h5>
+    </div>
+    <p class="mb-1 text-center"><?= $row['item_subcategory']; ?></p>
+  </a> 
+    <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+    <div class="d-flex w-100 justify-content-center">
+      <h5 class="mb-1">Price in store:</h5>
+    </div>
+    <p class="mb-1 text-center"> RM <?= $row['price_in_store']; ?></p>
+  </a>  
+    <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+    <div class="d-flex w-100 justify-content-center">
+      <h5 class="mb-1">Description:</h5>
+    </div>
+    <p class="mb-1 text-center"><?= $row['description']; ?></p>
+  </a>
+</div>
+<?php
+	}
+    }else{
+		  echo "<br>";
+          echo "<p class='fw-bold fs-4 text-center'>No Record Found</p>";}
+    }
+?>
+</div>
---
+++ visitor_encyclopedia_interface.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "page_head.php";?>
+    <title>Gardening Encyclopedia</title>
+	  <style>
+      .card:hover{
+        box-shadow: 5px 6px 6px 2px #e9ecef;
+        transform: scale(1.1);
+      }
+    </style>
+</head>
+<body>
+  <?php include 'header.php'; ?>
+
+    <div class="container border border-success rounded my-5">
+      <div class="row text-center">  
+        <div class="col-4 w-100">
+          <h1 class="text-uppercase my-4">Gardening Encyclopedia</h1>
+		  	  <img src="images/Cactus8.jpg" class="w-50">
+		<br>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="w-75 my-4">
+          <p class="fs-5 fw-bold">Explore plants, gardening tools and fertilizer</p>
+		  <p>
+		Garden tools are an essential component of garden supplies. 
+		The right garden tools can make gardening simple and productive.  
+		Garden tools are available for the various tasks associated with gardening. 
+		Pest control necessitates the use of various tools, such as sprayers and possibly protective masks. 
+		At Cacti Succulent Kuching, protective clothing, goggles,a hat and chemical sprays are available in store.
+		Other than that, chemical products, such as fungicides, fertilisers, and insect sprays can be found in store too. 
+		</p>
+        </div>
+
+<div class="card-group mb-3">
+  <div class="card mx-5" style="cursor: pointer;" onclick="window.location.href='plant_encyclopedia.php'">
+    <img class="card-img-top" src="images/Plants.jpeg" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Plants</h5>
+      <p class="card-text">
+		Plants are multicellular organisms in the kingdom Plantae that produce their own food through photosynthesis.  
+		Plants play an important role in the global ecosystem. 
+		They produce the majority of the world's oxygen and play an important role in the food chain because many organisms eat plants or organisms that eat plants. 
+	  </p>
+    </div>
+  </div>
+   <div class="card" style="cursor: pointer;" onclick="window.location.href='gardeningtools_encyclopedia.php'">
+    <img class="card-img-top" src="images/tools.jpeg" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Gardening Tools</h5>
+      <p class="card-text">
+		A gardening tool or garden tool is any of the many tools designed for gardens and gardening, which overlap with the range of tools designed for agriculture and horticulture. 
+		Garden tools can be both hand tools and power tools.
+	  </p>
+    </div>
+  </div>
+   <div class="card mx-5" style="cursor: pointer;" onclick="window.location.href='fertilizer_encyclopedia.php'">
+    <img class="card-img-top" src="images/fertilizer.jpeg" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Fertilizer</h5>
+      <p class="card-text">
+		Fertiliser is a natural or synthetic material that is added to the soil or plants to promote growth and productivity. 
+		They provide nutrients to the plants.
+	  </p>
+    </div>
+  </div>
+</div>
+      </div>
+	  <br>
+    </div>
+<?php include 'footer.php'; ?>
+</body>
+</html>

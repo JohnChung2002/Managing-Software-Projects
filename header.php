76d81ae0758dff4ce91bd3c43275f79cf93d8b19
John Chung <101917927+JohnChung2002@users.noreply.github.com>
2022-11-14 14:18:50 +0800

    Update header.php

--- header.php
+++ header.php
@@ -83,7 +83,7 @@ if ($logged_in) {
   if ($user_role == 'Super Admin') {
     echo '
     <li class="nav-item dropdown me-5">
-      <a class="nav-link mt-2 me-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      <a class="nav-link me-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
         <p class="dropdown-toggle fs-5 mx-3 mt-3" style="color: white;">Account Management</p>
       </a>
       <ul class="dropdown-menu">

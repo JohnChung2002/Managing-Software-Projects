69d0f72802fe9770658e3f42767fdcfab19adb77
Gabriel Kuek <101234169@students.swinburne.edu.my>
2022-11-14 13:00:07 +0800

    Updated the onclick function for delete button

--- profilepage-edit.php
+++ profilepage-edit.php
@@ -91,7 +91,7 @@
                 <button type="submit" class="btn btn-primary">Save Changes</button>
                 <?php
                     if($_SESSION['user_role'] == "User"){
-                    echo '<button type="button" class="btn btn-outline-danger float-end" onclick="location.href="deleteaccount-user.php";">Delete Account</button>';}
+                    echo "<button type='button' class='btn btn-outline-danger float-end' onclick='location.href=\"deleteaccount-user.php\";'>Delete Account</button>";}
                 ?>
             </fieldset>
         </form>
